881534bb1d08776232dff8f1d9a9af2bed58e468
kalille Overton <>
2021-08-21 00:31:47 -0400

    completed assignment

---
+++ reverse_each_word.rb
@@ -0,0 +1,16 @@
+
+require 'pry'
+def reverse_each_word(string)
+   
+   string.split.map{|word| 
+     word.reverse}.join(" ")
+
+
+end
+def reverse_each_word(string)
+   
+    string.split.collect{|word| 
+      word.reverse}.join(" ")
+ 
+ 
+ end
